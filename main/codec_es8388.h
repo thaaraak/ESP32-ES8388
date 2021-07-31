e432c751299f8017f2934fcd2c724a6e44d60126
thaaraak <ghangrene@gmail.com>
2021-07-31 14:01:34 -0500

    added

---
+++ main/codec_es8388.h
@@ -0,0 +1,234 @@
+/*
+ * es8388.h
+ *
+ *  Created on: Jul 31, 2021
+ *      Author: xenir
+ */
+
+#ifndef MAIN_CODEC_ES8388_H_
+#define MAIN_CODEC_ES8388_H_
+
+
+/* ES8388 register */
+#define ES8388_CONTROL1         0x00
+#define ES8388_CONTROL2         0x01
+
+#define ES8388_CHIPPOWER        0x02
+
+#define ES8388_ADCPOWER         0x03
+#define ES8388_DACPOWER         0x04
+
+#define ES8388_CHIPLOPOW1       0x05
+#define ES8388_CHIPLOPOW2       0x06
+
+#define ES8388_ANAVOLMANAG      0x07
+
+#define ES8388_MASTERMODE       0x08
+/* ADC */
+#define ES8388_ADCCONTROL1      0x09
+#define ES8388_ADCCONTROL2      0x0a
+#define ES8388_ADCCONTROL3      0x0b
+#define ES8388_ADCCONTROL4      0x0c
+#define ES8388_ADCCONTROL5      0x0d
+#define ES8388_ADCCONTROL6      0x0e
+#define ES8388_ADCCONTROL7      0x0f
+#define ES8388_ADCCONTROL8      0x10
+#define ES8388_ADCCONTROL9      0x11
+#define ES8388_ADCCONTROL10     0x12
+#define ES8388_ADCCONTROL11     0x13
+#define ES8388_ADCCONTROL12     0x14
+#define ES8388_ADCCONTROL13     0x15
+#define ES8388_ADCCONTROL14     0x16
+/* DAC */
+#define ES8388_DACCONTROL1      0x17
+#define ES8388_DACCONTROL2      0x18
+#define ES8388_DACCONTROL3      0x19
+#define ES8388_DACCONTROL4      0x1a
+#define ES8388_DACCONTROL5      0x1b
+#define ES8388_DACCONTROL6      0x1c
+#define ES8388_DACCONTROL7      0x1d
+#define ES8388_DACCONTROL8      0x1e
+#define ES8388_DACCONTROL9      0x1f
+#define ES8388_DACCONTROL10     0x20
+#define ES8388_DACCONTROL11     0x21
+#define ES8388_DACCONTROL12     0x22
+#define ES8388_DACCONTROL13     0x23
+#define ES8388_DACCONTROL14     0x24
+#define ES8388_DACCONTROL15     0x25
+#define ES8388_DACCONTROL16     0x26
+#define ES8388_DACCONTROL17     0x27
+#define ES8388_DACCONTROL18     0x28
+#define ES8388_DACCONTROL19     0x29
+#define ES8388_DACCONTROL20     0x2a
+#define ES8388_DACCONTROL21     0x2b
+#define ES8388_DACCONTROL22     0x2c
+#define ES8388_DACCONTROL23     0x2d
+#define ES8388_DACCONTROL24     0x2e
+#define ES8388_DACCONTROL25     0x2f
+#define ES8388_DACCONTROL26     0x30
+#define ES8388_DACCONTROL27     0x31
+#define ES8388_DACCONTROL28     0x32
+#define ES8388_DACCONTROL29     0x33
+#define ES8388_DACCONTROL30     0x34
+
+typedef enum {
+    I2S_NORMAL = 0,  /*!< set normal I2S format */
+    I2S_LEFT,        /*!< set all left format */
+    I2S_RIGHT,       /*!< set all right format */
+    I2S_DSP,         /*!< set dsp/pcm format */
+} es_format_t;
+
+typedef enum {
+    BIT_LENGTH_MIN = -1,
+    BIT_LENGTH_16BITS = 0x03,
+    BIT_LENGTH_18BITS = 0x02,
+    BIT_LENGTH_20BITS = 0x01,
+    BIT_LENGTH_24BITS = 0x00,
+    BIT_LENGTH_32BITS = 0x04,
+    BIT_LENGTH_MAX,
+} es_bits_length_t;
+
+typedef enum {
+    MCLK_DIV_MIN = -1,
+    MCLK_DIV_1 = 1,
+    MCLK_DIV_2 = 2,
+    MCLK_DIV_3 = 3,
+    MCLK_DIV_4 = 4,
+    MCLK_DIV_6 = 5,
+    MCLK_DIV_8 = 6,
+    MCLK_DIV_9 = 7,
+    MCLK_DIV_11 = 8,
+    MCLK_DIV_12 = 9,
+    MCLK_DIV_16 = 10,
+    MCLK_DIV_18 = 11,
+    MCLK_DIV_22 = 12,
+    MCLK_DIV_24 = 13,
+    MCLK_DIV_33 = 14,
+    MCLK_DIV_36 = 15,
+    MCLK_DIV_44 = 16,
+    MCLK_DIV_48 = 17,
+    MCLK_DIV_66 = 18,
+    MCLK_DIV_72 = 19,
+    MCLK_DIV_5 = 20,
+    MCLK_DIV_10 = 21,
+    MCLK_DIV_15 = 22,
+    MCLK_DIV_17 = 23,
+    MCLK_DIV_20 = 24,
+    MCLK_DIV_25 = 25,
+    MCLK_DIV_30 = 26,
+    MCLK_DIV_32 = 27,
+    MCLK_DIV_34 = 28,
+    MCLK_DIV_7  = 29,
+    MCLK_DIV_13 = 30,
+    MCLK_DIV_14 = 31,
+    MCLK_DIV_MAX,
+} es_sclk_div_t;
+
+typedef enum {
+    LCLK_DIV_MIN = -1,
+    LCLK_DIV_128 = 0,
+    LCLK_DIV_192 = 1,
+    LCLK_DIV_256 = 2,
+    LCLK_DIV_384 = 3,
+    LCLK_DIV_512 = 4,
+    LCLK_DIV_576 = 5,
+    LCLK_DIV_768 = 6,
+    LCLK_DIV_1024 = 7,
+    LCLK_DIV_1152 = 8,
+    LCLK_DIV_1408 = 9,
+    LCLK_DIV_1536 = 10,
+    LCLK_DIV_2112 = 11,
+    LCLK_DIV_2304 = 12,
+
+    LCLK_DIV_125 = 16,
+    LCLK_DIV_136 = 17,
+    LCLK_DIV_250 = 18,
+    LCLK_DIV_272 = 19,
+    LCLK_DIV_375 = 20,
+    LCLK_DIV_500 = 21,
+    LCLK_DIV_544 = 22,
+    LCLK_DIV_750 = 23,
+    LCLK_DIV_1000 = 24,
+    LCLK_DIV_1088 = 25,
+    LCLK_DIV_1496 = 26,
+    LCLK_DIV_1500 = 27,
+    LCLK_DIV_MAX,
+} es_lclk_div_t;
+
+typedef enum {
+    D2SE_PGA_GAIN_MIN = -1,
+    D2SE_PGA_GAIN_DIS = 0,
+    D2SE_PGA_GAIN_EN = 1,
+    D2SE_PGA_GAIN_MAX = 2,
+} es_d2se_pga_t;
+
+typedef enum {
+    ADC_INPUT_MIN = -1,
+    ADC_INPUT_LINPUT1_RINPUT1 = 0x00,
+    ADC_INPUT_MIC1  = 0x05,
+    ADC_INPUT_MIC2  = 0x06,
+    ADC_INPUT_LINPUT2_RINPUT2 = 0x50,
+    ADC_INPUT_DIFFERENCE = 0xf0,
+    ADC_INPUT_MAX,
+} es_adc_input_t;
+
+typedef enum {
+    DAC_OUTPUT_MIN = -1,
+    DAC_OUTPUT_LOUT1 = 0x04,
+    DAC_OUTPUT_LOUT2 = 0x08,
+    DAC_OUTPUT_SPK   = 0x09,
+    DAC_OUTPUT_ROUT1 = 0x10,
+    DAC_OUTPUT_ROUT2 = 0x20,
+    DAC_OUTPUT_ALL = 0x3c,
+    DAC_OUTPUT_MAX,
+} es_dac_output_t;
+
+typedef enum {
+    MIC_GAIN_MIN = -1,
+    MIC_GAIN_0DB = 0,
+    MIC_GAIN_3DB = 3,
+    MIC_GAIN_6DB = 6,
+    MIC_GAIN_9DB = 9,
+    MIC_GAIN_12DB = 12,
+    MIC_GAIN_15DB = 15,
+    MIC_GAIN_18DB = 18,
+    MIC_GAIN_21DB = 21,
+    MIC_GAIN_24DB = 24,
+    MIC_GAIN_MAX,
+} es_mic_gain_t;
+
+typedef enum {
+    ES_MODULE_MIN = -1,
+    ES_MODULE_ADC = 0x01,
+    ES_MODULE_DAC = 0x02,
+    ES_MODULE_ADC_DAC = 0x03,
+    ES_MODULE_LINE = 0x04,
+    ES_MODULE_MAX
+} es_module_t;
+
+typedef enum {
+    ES_MODE_MIN = -1,
+    ES_MODE_SLAVE = 0x00,
+    ES_MODE_MASTER = 0x01,
+    ES_MODE_MAX,
+} es_mode_t;
+
+typedef enum {
+    ES_I2S_MIN = -1,
+    ES_I2S_NORMAL = 0,
+    ES_I2S_LEFT = 1,
+    ES_I2S_RIGHT = 2,
+    ES_I2S_DSP = 3,
+    ES_I2S_MAX
+} es_i2s_fmt_t;
+
+/**
+ * @brief Configure ES8388 clock
+ */
+typedef struct {
+    es_sclk_div_t sclk_div;    /*!< bits clock divide */
+    es_lclk_div_t lclk_div;    /*!< WS clock divide */
+} es_i2s_clock_t;
+
+
+#endif /* MAIN_CODEC_ES8388_H_ */
---
+++ main/main.c
@@ -0,0 +1,371 @@
+#include "freertos/FreeRTOS.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+#include "driver/i2c.h"
+#include "driver/gpio.h"
+#include "driver/i2s.h"
+#include "soc/gpio_sig_map.h"
+#include "codec_es8388.h"
+#include "math.h"
+
+#define I2C_MASTER_NUM	0
+#define ACK_CHECK_EN 0x1
+#define ACK_CHECK_DIS 0x0
+#define ACK_VAL I2C_MASTER_ACK
+#define NACK_VAL I2C_MASTER_NACK
+
+#define ES8388_ADDR 0b0010000
+#define I2C_MASTER_SDA_IO 22
+#define I2C_MASTER_SCL_IO 23
+
+
+#define SAMPLE_RATE     (44100)
+#define I2S_NUM         (0)
+#define WAVE_FREQ_HZ    (700)
+#define PI              (3.14159265)
+#define I2S_BCK_IO      (GPIO_NUM_5)
+#define I2S_WS_IO       (GPIO_NUM_18)
+#define I2S_DO_IO       (GPIO_NUM_21)
+#define I2S_DI_IO       (GPIO_NUM_19)
+
+static const char *ES_TAG = "ES8388_DRIVER";
+
+
+esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    return ESP_OK;
+}
+
+uint8_t i2c_write_bulk( uint8_t i2c_bus_addr, uint8_t reg, uint8_t bytes, uint8_t *data)
+{
+    printf( "Writing [%02x]=", reg );
+    for ( int i = 0 ; i < bytes ; i++ )
+        printf( "%02x:", data[i] );
+    printf( "\n");
+
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, i2c_bus_addr << 1 | I2C_MASTER_WRITE, ACK_CHECK_EN);
+    i2c_master_write(cmd, &reg, 1, ACK_CHECK_EN);
+    i2c_master_write(cmd, data, bytes, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    int ret = i2c_master_cmd_begin( I2C_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    if (ret != ESP_OK) {
+        return ret;
+    }
+
+    return 0;
+}
+
+uint8_t i2c_write( uint8_t i2c_bus_addr, uint8_t reg, uint8_t value)
+{
+    return i2c_write_bulk( i2c_bus_addr, reg, 1, &value );
+}
+
+
+
+uint8_t i2c_read( uint8_t i2c_bus_addr, uint8_t reg)
+{
+	   	uint8_t buffer[2];
+	    //printf( "Addr: [%d] Reading register: [%d]\n", i2c_bus_addr, reg );
+
+	    buffer[0] = reg;
+
+	    int ret;
+	    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+
+	    // Write the register address to be read
+	    i2c_master_start(cmd);
+	    i2c_master_write_byte(cmd, i2c_bus_addr << 1 | I2C_MASTER_WRITE, ACK_CHECK_EN);
+	    i2c_master_write_byte(cmd, buffer[0], ACK_CHECK_EN);
+
+	    // Read the data for the register from the slave
+	    i2c_master_start(cmd);
+	    i2c_master_write_byte(cmd, i2c_bus_addr << 1 | I2C_MASTER_READ, ACK_CHECK_EN);
+	    i2c_master_read_byte(cmd, &buffer[0], NACK_VAL);
+	    i2c_master_stop(cmd);
+
+	    ret = i2c_master_cmd_begin( I2C_MASTER_NUM, cmd, 1000 / portTICK_RATE_MS);
+	    i2c_cmd_link_delete(cmd);
+
+	    //printf( "Read: [%d]\n", buffer[0] );
+
+	    return (buffer[0]);
+}
+
+
+static esp_err_t i2c_master_init(void)
+{
+    int i2c_master_port = I2C_MASTER_NUM;
+    i2c_config_t conf;
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_MASTER_SDA_IO;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_io_num = I2C_MASTER_SCL_IO;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = 100000;
+
+    i2c_param_config(i2c_master_port, &conf);
+
+    return i2c_driver_install(i2c_master_port, conf.mode, 0, 0, 0);
+}
+
+static esp_err_t es_write_reg(uint8_t slave_addr, uint8_t reg_add, uint8_t data)
+{
+    return i2c_write( ES8388_ADDR, reg_add, data );
+}
+
+static esp_err_t es_read_reg(uint8_t reg_add, uint8_t *p_data)
+{
+    *p_data = i2c_read( ES8388_ADDR, reg_add );
+    return ESP_OK;
+}
+
+
+esp_err_t es8388_init( es_dac_output_t output, es_adc_input_t input )
+{
+    int res = 0;
+
+    res |= es_write_reg(ES8388_ADDR, ES8388_DACCONTROL3, 0x04);  // 0x04 mute/0x00 unmute&ramp;DAC unmute and  disabled digital volume control soft ramp
+
+    res |= es_write_reg(ES8388_ADDR, ES8388_CONTROL2, 0x50);
+    res |= es_write_reg(ES8388_ADDR, ES8388_CHIPPOWER, 0x00); //normal all and power up all
+    res |= es_write_reg(ES8388_ADDR, ES8388_MASTERMODE, ES_MODE_SLAVE ); //CODEC IN I2S SLAVE MODE
+
+    res |= es_write_reg(ES8388_ADDR, ES8388_DACPOWER, 0xC0);  //disable DAC and disable Lout/Rout/1/2
+    res |= es_write_reg(ES8388_ADDR, ES8388_CONTROL1, 0x12);  //Enfr=0,Play&Record Mode,(0x17-both of mic&paly)
+    res |= es_write_reg(ES8388_ADDR, ES8388_DACCONTROL1, 0x18);//1a 0x18:16bit iis , 0x00:24
+    res |= es_write_reg(ES8388_ADDR, ES8388_DACCONTROL2, 0x02);  //DACFsMode,SINGLE SPEED; DACFsRatio,256
+    res |= es_write_reg(ES8388_ADDR, ES8388_DACCONTROL16, 0x00); // 0x00 audio on LIN1&RIN1,  0x09 LIN2&RIN2
+    res |= es_write_reg(ES8388_ADDR, ES8388_DACCONTROL17, 0x90); // only left DAC to left mixer enable 0db
+    res |= es_write_reg(ES8388_ADDR, ES8388_DACCONTROL20, 0x90); // only right DAC to right mixer enable 0db
+    res |= es_write_reg(ES8388_ADDR, ES8388_DACCONTROL21, 0x80); //set internal ADC and DAC use the same LRCK clock, ADC LRCK as internal LRCK
+    res |= es_write_reg(ES8388_ADDR, ES8388_DACCONTROL23, 0x00);   //vroi=0
+    //res |= es8388_set_adc_dac_volume(ES_MODULE_DAC, 0, 0);          // 0db
+
+    ESP_LOGE(ES_TAG, "Setting DAC Output: %02x", output );
+    res |= es_write_reg(ES8388_ADDR, ES8388_DACPOWER, output );
+    res |= es_write_reg(ES8388_ADDR, ES8388_ADCPOWER, 0xFF);
+    res |= es_write_reg(ES8388_ADDR, ES8388_ADCCONTROL1, 0xbb); // MIC Left and Right channel PGA gain
+
+
+    ESP_LOGE(ES_TAG, "Setting ADC Input: %02x", input );
+    res |= es_write_reg(ES8388_ADDR, ES8388_ADCCONTROL2, input);
+
+    res |= es_write_reg(ES8388_ADDR, ES8388_ADCCONTROL3, 0x02);
+    res |= es_write_reg(ES8388_ADDR, ES8388_ADCCONTROL4, 0x0d); // Left/Right data, Left/Right justified mode, Bits length, I2S format
+    res |= es_write_reg(ES8388_ADDR, ES8388_ADCCONTROL5, 0x02);  //ADCFsMode,singel SPEED,RATIO=256
+    //ALC for Microphone
+    //res |= es8388_set_adc_dac_volume(ES_MODULE_ADC, 0, 0);      // 0db
+    res |= es_write_reg(ES8388_ADDR, ES8388_ADCPOWER, 0x09); //Power on ADC, Enable LIN&RIN, Power off MICBIAS, set int1lp to low power mode
+
+    return res;
+}
+
+// This function sets the I2S format which can be one of
+//		I2S_NORMAL
+//		I2S_LEFT		Left Justified
+//		I2S_RIGHT,      Right Justified
+//		I2S_DSP,        dsp/pcm format
+//
+// and the bits per sample which must be one of
+//		BIT_LENGTH_16BITS
+//		BIT_LENGTH_18BITS
+//		BIT_LENGTH_20BITS
+//		BIT_LENGTH_24BITS
+//		BIT_LENGTH_32BITS
+//
+// Note the above must match the ESP-IDF I2S configuration which is set separately
+
+esp_err_t es8388_config_i2s( es_bits_length_t bits_length, es_module_t mode, es_format_t fmt )
+{
+    esp_err_t res = ESP_OK;
+    uint8_t reg = 0;
+
+    // Set the Format
+    if (mode == ES_MODULE_ADC || mode == ES_MODULE_ADC_DAC) {
+        res = es_read_reg(ES8388_ADCCONTROL4, &reg);
+        reg = reg & 0xfc;
+        res |= es_write_reg(ES8388_ADDR, ES8388_ADCCONTROL4, reg | fmt);
+    }
+    if (mode == ES_MODULE_DAC || mode == ES_MODULE_ADC_DAC) {
+        res = es_read_reg(ES8388_DACCONTROL1, &reg);
+        reg = reg & 0xf9;
+        res |= es_write_reg(ES8388_ADDR, ES8388_DACCONTROL1, reg | (fmt << 1));
+    }
+
+
+    // Set the Sample bits length
+    int bits = (int)bits_length;
+    if (mode == ES_MODULE_ADC || mode == ES_MODULE_ADC_DAC) {
+        res = es_read_reg(ES8388_ADCCONTROL4, &reg);
+        reg = reg & 0xe3;
+        res |=  es_write_reg(ES8388_ADDR, ES8388_ADCCONTROL4, reg | (bits << 2));
+    }
+    if (mode == ES_MODULE_DAC || mode == ES_MODULE_ADC_DAC) {
+        res = es_read_reg(ES8388_DACCONTROL1, &reg);
+        reg = reg & 0xc7;
+        res |= es_write_reg(ES8388_ADDR, ES8388_DACCONTROL1, reg | (bits << 3));
+    }
+    return res;
+}
+
+
+esp_err_t es8388_set_voice_mute(bool enable)
+{
+    esp_err_t res = ESP_OK;
+    uint8_t reg = 0;
+    res = es_read_reg(ES8388_DACCONTROL3, &reg);
+    reg = reg & 0xFB;
+    res |= es_write_reg(ES8388_ADDR, ES8388_DACCONTROL3, reg | (((int)enable) << 2));
+    return res;
+}
+
+esp_err_t es8388_start(es_module_t mode)
+{
+    esp_err_t res = ESP_OK;
+    uint8_t prev_data = 0, data = 0;
+    es_read_reg(ES8388_DACCONTROL21, &prev_data);
+    if (mode == ES_MODULE_LINE) {
+        res |= es_write_reg(ES8388_ADDR, ES8388_DACCONTROL16, 0x09); // 0x00 audio on LIN1&RIN1,  0x09 LIN2&RIN2 by pass enable
+        res |= es_write_reg(ES8388_ADDR, ES8388_DACCONTROL17, 0x50); // left DAC to left mixer enable  and  LIN signal to left mixer enable 0db  : bupass enable
+        res |= es_write_reg(ES8388_ADDR, ES8388_DACCONTROL20, 0x50); // right DAC to right mixer enable  and  LIN signal to right mixer enable 0db : bupass enable
+        res |= es_write_reg(ES8388_ADDR, ES8388_DACCONTROL21, 0xC0); //enable adc
+    } else {
+        res |= es_write_reg(ES8388_ADDR, ES8388_DACCONTROL21, 0x80);   //enable dac
+    }
+    es_read_reg(ES8388_DACCONTROL21, &data);
+    if (prev_data != data) {
+        res |= es_write_reg(ES8388_ADDR, ES8388_CHIPPOWER, 0xF0);   //start state machine
+        // res |= es_write_reg(ES8388_ADDR, ES8388_CONTROL1, 0x16);
+        // res |= es_write_reg(ES8388_ADDR, ES8388_CONTROL2, 0x50);
+        res |= es_write_reg(ES8388_ADDR, ES8388_CHIPPOWER, 0x00);   //start state machine
+    }
+    if (mode == ES_MODULE_ADC || mode == ES_MODULE_ADC_DAC || mode == ES_MODULE_LINE) {
+        res |= es_write_reg(ES8388_ADDR, ES8388_ADCPOWER, 0x00);   //power up adc and line in
+    }
+    if (mode == ES_MODULE_DAC || mode == ES_MODULE_ADC_DAC || mode == ES_MODULE_LINE) {
+        res |= es_write_reg(ES8388_ADDR, ES8388_DACPOWER, 0x3c);   //power up dac and line out
+        res |= es8388_set_voice_mute(false);
+        ESP_LOGD(ES_TAG, "es8388_start default is mode:%d", mode);
+    }
+
+    return res;
+}
+
+
+void es8388_config()
+{
+    // Input/Output Modes
+    //
+    //	es_dac_output_t output = DAC_OUTPUT_LOUT1 | DAC_OUTPUT_LOUT2 | DAC_OUTPUT_ROUT1 | DAC_OUTPUT_ROUT2;
+    //	es_adc_input_t input = ADC_INPUT_LINPUT1_RINPUT1;
+    // 	es_adc_input_t input = ADC_INPUT_LINPUT2_RINPUT2;
+
+    es_dac_output_t output = DAC_OUTPUT_LOUT1 | DAC_OUTPUT_LOUT2 | DAC_OUTPUT_ROUT1 | DAC_OUTPUT_ROUT2;
+    es_adc_input_t input = ADC_INPUT_LINPUT1_RINPUT1;
+
+    es8388_init( output, input );
+
+    // Modes Available
+    //
+    //	es_mode_t  = ES_MODULE_ADC;
+    //	es_mode_t  = ES_MODULE_LINE;
+    //	es_mode_t  = ES_MODULE_DAC;
+    //	es_mode_t  = ES_MODULE_ADC_DAC;
+
+    es_bits_length_t bits_length = BIT_LENGTH_16BITS;
+    es_module_t module = ES_MODULE_ADC_DAC;
+    es_format_t fmt = I2S_NORMAL;
+
+    es8388_config_i2s( bits_length, module, fmt );
+
+    es8388_start( module );
+
+
+}
+
+esp_err_t i2s_mclk_gpio_select(i2s_port_t i2s_num, gpio_num_t gpio_num)
+{
+
+	// Ignore whatever is sent in and fix to Pin 3
+
+	gpio_num = GPIO_NUM_3;
+
+    if (i2s_num >= I2S_NUM_MAX) {
+        ESP_LOGE(ES_TAG, "Does not support i2s number(%d)", i2s_num);
+        return ESP_ERR_INVALID_ARG;
+    }
+    if (gpio_num != GPIO_NUM_0 && gpio_num != GPIO_NUM_1 && gpio_num != GPIO_NUM_3) {
+        ESP_LOGE(ES_TAG, "Only support GPIO0/GPIO1/GPIO3, gpio_num:%d", gpio_num);
+        return ESP_ERR_INVALID_ARG;
+    }
+    ESP_LOGI(ES_TAG, "I2S%d, MCLK output by GPIO%d", i2s_num, gpio_num);
+    if (i2s_num == I2S_NUM_0) {
+        if (gpio_num == GPIO_NUM_0) {
+            PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO0_U, FUNC_GPIO0_CLK_OUT1);
+            WRITE_PERI_REG(PIN_CTRL, 0xFFF0);
+        } else if (gpio_num == GPIO_NUM_1) {
+            PIN_FUNC_SELECT(PERIPHS_IO_MUX_U0TXD_U, FUNC_U0TXD_CLK_OUT3);
+            WRITE_PERI_REG(PIN_CTRL, 0xF0F0);
+        } else {
+            PIN_FUNC_SELECT(PERIPHS_IO_MUX_U0RXD_U, FUNC_U0RXD_CLK_OUT2);
+            WRITE_PERI_REG(PIN_CTRL, 0xFF00);
+        }
+    } else if (i2s_num == I2S_NUM_1) {
+        if (gpio_num == GPIO_NUM_0) {
+            PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO0_U, FUNC_GPIO0_CLK_OUT1);
+            WRITE_PERI_REG(PIN_CTRL, 0xFFFF);
+        } else if (gpio_num == GPIO_NUM_1) {
+            PIN_FUNC_SELECT(PERIPHS_IO_MUX_U0TXD_U, FUNC_U0TXD_CLK_OUT3);
+            WRITE_PERI_REG(PIN_CTRL, 0xF0FF);
+        } else {
+            PIN_FUNC_SELECT(PERIPHS_IO_MUX_U0RXD_U, FUNC_U0RXD_CLK_OUT2);
+            WRITE_PERI_REG(PIN_CTRL, 0xFF0F);
+        }
+    }
+    return ESP_OK;
+}
+
+
+
+void i2s_init()
+{
+
+	i2s_config_t i2s_config = {
+        .mode = I2S_MODE_MASTER | I2S_MODE_TX,
+        .sample_rate = SAMPLE_RATE,
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+        .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+        .communication_format = I2S_COMM_FORMAT_STAND_MSB,
+        .dma_buf_count = 3,
+        .dma_buf_len = 300,
+        .use_apll = true,
+	    .tx_desc_auto_clear = true,
+	    .fixed_mclk = 0,
+        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1
+    };
+
+    i2s_pin_config_t pin_config = {
+        .bck_io_num = I2S_BCK_IO,
+        .ws_io_num = I2S_WS_IO,
+        .data_out_num = I2S_DO_IO,
+        .data_in_num = I2S_DI_IO                                               //Not used
+    };
+    i2s_driver_install(I2S_NUM, &i2s_config, 0, NULL);
+    i2s_set_pin(I2S_NUM, &pin_config);
+    i2s_mclk_gpio_select(I2S_NUM, (gpio_num_t)GPIO_NUM_3 );
+    i2s_set_clk(I2S_NUM, SAMPLE_RATE, 16, 2);
+
+}
+
+void app_main(void)
+{
+	i2c_master_init();
+	es8388_config();
+	i2s_init();
+}
+
